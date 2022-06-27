e8220816ff126c182855454f6c37c858812ca0d4
emanuelhardwell <emanuelnoe16@gmail.com>
2022-06-26 22:10:52 -0500

    Se agrego el Schema de Categoria para validar

---
+++ src/schemas/category.schema.ts
@@ -0,0 +1,34 @@
+import { z } from "zod";
+import mongoose from "mongoose";
+import isMongoId from "validator/lib/isMongoId";
+
+export const createCategorySchema = z.object({
+  body: z.object({
+    name: z
+      .string({ required_error: "Categoria requerida" })
+      .min(3, "La categoria debe tener más de 2 letras")
+      .trim(),
+  }),
+});
+
+export const updateCategorySchema = z.object({
+  body: z.object({
+    name: z
+      .string({ required_error: "Categoria requerida" })
+      .min(3, "La categoria debe tener más de 2 letras")
+      .trim(),
+  }),
+  params: z.object({
+    id: z.string().refine(isMongoId, {
+      message: "Id no valido",
+    }),
+  }),
+});
+
+export const deleteCategorySchema = z.object({
+  params: z.object({
+    id: z.string().refine(isMongoId, {
+      message: "Id no valido",
+    }),
+  }),
+});
